97b771b0d9bb7065500edc399964a6a09a211d9f
mausam khatiwada <mausamkhatiwada98@gmail.com>
2023-09-14 15:44:41 +0545

    A word Scramble game

---
+++ script.js
@@ -0,0 +1,73 @@
+const wordText = document.querySelector(".word");
+const hintText = document.querySelector(".hint");
+const giveup = document.querySelector(".giveup");
+const input = document.getElementById("text");
+const submit = document.querySelector(".submit");
+let correctScore = document.querySelector(".correctScore");
+let wrongScore = document.querySelector(".wrongScore");
+let timeText = document.querySelector(".time p");
+
+let correctWord,time;
+const timer = maxTime =>{
+	clearInterval(time);
+time = setInterval(() =>{
+	if (maxTime > 0) {
+		maxTime --;
+	return timeText.innerText =maxTime + "s";
+	}
+	if (maxTime == 0) {
+			j=j+1;
+	wrongScore.innerText=j;
+	}
+clearInterval(time);
+initGame();
+
+}, 1000);
+}
+
+const initGame = () =>{
+	timer(15);
+	let randomObj = scrambleWords[Math.floor(Math.random()*scrambleWords.length)];
+
+	let wordArray = randomObj.word.split("");
+
+	for (var i = wordArray.length - 1; i > 0; i--) {
+		var j = Math.floor(Math.random()* (i+1));
+		[wordArray[i], wordArray[j]] = 	[wordArray[j], wordArray[i]]
+	}
+	wordText.innerText= wordArray.join("");
+	hintText.innerText = randomObj.hint;
+	correctWord = randomObj.word.toLowerCase();
+	console.log(wordArray, randomObj.word)
+}
+
+giveup.addEventListener("click",()=>{
+
+	j=j+1;
+	wrongScore.innerText=j;
+
+initGame();
+});
+let i = 0; let j = 0;
+submit.addEventListener("click",()=>{
+let userWord = input.value.toLocaleLowerCase();
+if (userWord==correctWord) {
+	i=i+1;
+	correctScore.innerText=i;
+}
+else{
+	j=j+1;
+	wrongScore.innerText=j;
+}
+	initGame();
+
+});
+
+
+
+
+
+
+
+
+initGame();
